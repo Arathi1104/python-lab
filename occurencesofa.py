44c1d60ff3b43088d30da2bc447f55520530a97c
Arathi1104 <arathik2711@gmail.com>
2025-10-25 14:22:44 +0530

    Refactor occurrence counting for case sensitivity

--- occurencesofa.py
+++ occurencesofa.py
@@ -1,12 +1,11 @@
-names=["Anu","Meena","Rani","Anand"]
-list=[]
+names = ["Anu", "Meena", "Rani", "Anand"]
+count = 0
+
 for i in names:
-    i=i.lower()
-    count=0
     for j in i:
-        list.append(j)
-for k in list:
-    if k=='a':
-        count+=1
-print("Totaloccurences of 'a':",count)
+        if j == 'a' or j == 'A':
+            count += 1
+
+print("Total occurrences of 'a':", count)
     
+
